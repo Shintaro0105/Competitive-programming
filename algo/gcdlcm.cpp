f9d03deb80f18bc213c8e23dff1d8b56297d0dcb
Shintaro0105 <shintarou0105@gmail.com>
2024-03-03 19:44:49 +0900

    extgcd

--- algo/gcdlcm.cpp
+++ algo/gcdlcm.cpp
@@ -33,6 +33,17 @@ ll lcm(ll a, ll b){
     return a*b / gcd(a, b);
 }
 
+long long extGCD(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extGCD(b, a%b, y, x);
+    y -= a/b * x;
+    return d;
+}
+
 int main(){
     return 0;
 }
